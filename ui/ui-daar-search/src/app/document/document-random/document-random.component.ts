55b9b3c1efab182fbe255bb95704041c16ae221c
celrati <m.charif.achraf@gmail.com>
2020-02-18 12:10:57 +0100

    the first prototype of the app

---
+++ ui/ui-daar-search/src/app/document/document-random/document-random.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-document-random',
+  templateUrl: './document-random.component.html',
+  styleUrls: ['./document-random.component.css']
+})
+export class DocumentRandomComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
